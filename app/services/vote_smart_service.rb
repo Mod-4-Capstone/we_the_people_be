c4932fe70b4dd18863ed69f56da838f5582bea4d
Michael Brandt <msbrandt00@gmail.com>
2022-05-15 22:39:38 -0600

    Feat/Test: Retrieve bio info and SIG rating for politicians

--- app/services/vote_smart_service.rb
+++ app/services/vote_smart_service.rb
@@ -15,4 +15,25 @@ class VoteSmartService
     response = conn.get("Officials.getByOfficeTypeState?officeTypeId=C&stateId=#{state}")
     JSON.parse(response.body, symbolize_names: true)[:candidateList][:candidate].filter {|c| c[:officeTypeId] == "C"}
   end
+
+  def self.candidate_bio(id)
+    response = conn.get("CandidateBio.getBio?candidateId=#{id}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  
+  def self.detailed_bio(id)
+    response = conn.get("CandidateBio.getDetailedBio?candidateId=#{id}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  
+  def self.get_all_sig_ratings(id)
+    response = conn.get("Rating.getCandidateRating?candidateId=#{id}")
+    if response.body[-1] != "}"
+      # representatives return invalid JSON
+      response_body = response.body + '"}]}}'
+      parsed = JSON.parse(response_body, symbolize_names: true)
+    else 
+      parsed = JSON.parse(response.body, symbolize_names: true)
+    end
+  end
 end
