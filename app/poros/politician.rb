6466975944f3c20d600089f91c9db3c51cb0481b
Michael Brandt <msbrandt00@gmail.com>
2022-05-17 09:41:41 -0600

    Refactor Bio for varying JSON structure

--- app/poros/politician.rb
+++ app/poros/politician.rb
@@ -1,7 +1,7 @@
 class Politician
     attr_reader :ratings, :bio
-    def initialize(id, ratings = false, biography = false)
+    def initialize(id)
         @ratings = VoteSmartFacade.specific_candidate_ratings(id)
-        @bio = VoteSmartFacade
+        @bio = VoteSmartFacade.candidate_bio(id)
     end
 end
