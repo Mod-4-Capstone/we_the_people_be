2a96027f558c1143a6cdec8718be5e701470c72d
Michael Brandt <msbrandt00@gmail.com>
2022-05-18 13:05:04 -0600

    Feature: State with quiz results

--- app/controllers/api/v1/congress_controller.rb
+++ app/controllers/api/v1/congress_controller.rb
@@ -16,11 +16,29 @@ class Api::V1::CongressController < ApplicationController
               numbers_usa: params[:Q9], 
               planned_parenthood: params[:Q10]
            }
-    VoteSmartFacade.zip_with_quiz(params[:zipcode], quiz)
-
+            render json: {
+              politicians: politicians, 
+              summary_statistics: VoteSmartFacade.zip_with_quiz(params[:zipcode], quiz)
+            }   
   end
-
-  def state
-    render json: {data: 'state'}
-  end
+          
+  def state_with_quiz 
+    quiz = {
+              aclu: params[:Q1], 
+              americans_for_prosperity: params[:Q2], 
+              end_citizens_united: params[:Q3], 
+              national_assocation_of_police: params[:Q4], 
+              national_education_assocation: params[:Q5], 
+              national_parks_conservation: params[:Q6], 
+              norml: params[:Q7], 
+              nra: params[:Q8], 
+              numbers_usa: params[:Q9], 
+              planned_parenthood: params[:Q10]
+            }
+            politicians = VoteSmartFacade.state_with_quiz('CO', quiz)
+            render json: {
+              politicians: politicians, 
+              summary_statistics: VoteSmartFacade.summary_statistics(politicians, quiz)
+            }
+    end
 end
