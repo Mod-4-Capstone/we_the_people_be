c10ddf1b0fbef25c7f55a65dbd613bb30405d622
Michael Brandt <msbrandt00@gmail.com>
2022-05-18 14:40:36 -0600

    Refactor: remove unecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :test, only: [:show]
-      post '/zip_code', to: 'congress#zip_code'
-      get '/state', to: 'congress#state'
-      post '/zip_code_with_quiz', to: "congress#zip_code_with_quiz"
+      post '/zipcode_with_quiz', to: "congress#zipcode_with_quiz"
       post '/state_with_quiz', to: 'congress#state_with_quiz'
     end
   end
