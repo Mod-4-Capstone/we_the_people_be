eca6f093b8bc0e01461208fafc31852740e9d666
Michael Brandt <msbrandt00@gmail.com>
2022-05-18 13:05:40 -0600

    Feature: regional info summary statistics

--- app/poros/politician.rb
+++ app/poros/politician.rb
@@ -1,10 +1,10 @@
 class Politician
-    attr_reader :ratings, :bio, :user_ratings
+    attr_reader :ratings, :bio, :user_ratings, :compatibility
     def initialize(id, user_ratings=nil)
         @ratings = VoteSmartFacade.specific_candidate_ratings(id)
         @bio = VoteSmartFacade.candidate_bio(id)
         @user_ratings = run_analysis(@ratings, user_ratings) if user_ratings.nil? == false
-        @opinion_delta = difference_of_opinion(politician_opinion_sum, @user_ratings.values.sum) if user_ratings.nil? == false
+        @compatibility = difference_of_opinion(politician_opinion_sum, @user_ratings.values.sum) if user_ratings.nil? == false
     end
 
     def run_analysis(ratings, user_ratings)
@@ -20,7 +20,7 @@ class Politician
         @ratings.aclu.to_i + @ratings.americans_for_prosperity.to_i + @ratings.end_citizens_united.to_i + @ratings.national_assocation_of_police.to_i + @ratings.national_assocation_of_police.to_i + @ratings.national_education_assocation.to_i+ @ratings.norml.to_i + @ratings.nra.to_i+ @ratings.numbers_usa.to_i + @ratings.planned_parenthood.to_i
     end
 
-    def difference_of_opinion(politician, user)
-        ((politician.to_f - user)/politician).abs*100
+    def difference_of_opinion(politician, user) 
+        (politician.to_f - user)/politician.abs*100
     end
 end
