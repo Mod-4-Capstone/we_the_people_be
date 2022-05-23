2f87a4a47ddea3f486ba9aa07b1d2b00715c1837
Conor Barthel <conor.barthel@gmail.com>
2022-05-23 11:33:10 -0600

    Test: Sad path testing for state_with_quiz

--- spec/requests/api/v2/state_with_quiz_spec.rb
+++ spec/requests/api/v2/state_with_quiz_spec.rb
@@ -71,5 +71,42 @@ RSpec.describe "Congress Requests" do
                         :numbers_usa)
       expect(response_data.first[:attributes]).to have_key(:compatibility)
     end
+
+    it "sad path will return an error if there is no quiz" do
+      body = {
+      id: 42,
+      age: "20-29",
+      state: "AL"
+      }
+
+      post '/api/v2/state_with_quiz', params: body
+
+      expect(response.status).to eq(400)
+      response_data = JSON.parse(response.body, symbolize_names: true)
+      expect(response_data[:error]).to eq("please include quiz!")
+    end
+
+    it "sad path will return an error if there is no state" do
+      body = {
+      id: 42,
+      age: "20-29",
+      aclu: "100",
+      americans_for_prosperity: "20",
+      end_citizens_united: "80",
+      national_association_of_police: "80",
+      national_education_association: "100",
+      national_parks_conservation: "100",
+      norml: "92",
+      nra: "7",
+      numbers_usa: "0",
+      planned_parenthood: "100"
+      }
+
+      post '/api/v2/state_with_quiz', params: body
+
+      expect(response.status).to eq(400)
+      response_data = JSON.parse(response.body, symbolize_names: true)
+      expect(response_data[:error]).to eq("please include state!")
+    end
   end
 end
